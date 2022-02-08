9be3b59d1df1615cf94c0c8f552c6783d899e051
Omobolaji Koyi <omobolaji-oluwajuwon.koyi@deutschebahn.com>
2022-02-08 14:31:35 +0100

    unit tests

--- src/components/topMessage/topMessage.test.tsx
+++ src/components/topMessage/topMessage.test.tsx
@@ -1,20 +1,41 @@
 import React from 'react';
-import { render, screen, cleanup, fireEvent} from '@testing-library/react';
+import { render, screen, cleanup} from '@testing-library/react';
 import ReactDOM from 'react-dom';
 import '@testing-library/jest-dom/extend-expect';
 import renderer from 'react-test-renderer';
+import userEvent from '@testing-library/user-event';
 import TopMessage from './topMessage';
 
 afterEach(cleanup);
 
 describe('TopMessage Component', ()=> {
-    test('Expect componet to be created', async () => {
+    test('Expect component to be created', async () => {
         expect(TopMessage).toBeTruthy()
     });
     
     test('renders without crashing', async () => {
         const div = document.createElement('div');
         ReactDOM.render(<TopMessage />, div);
+    });
+
+    test('first top link called', ()=> {
+        const click = jest.fn();
+        render(
+            <div data-testid='TopMessage-link1' onClick={click}>contributing guideline</div>
+        )
+        userEvent.click(screen.getByTestId('TopMessage-link1'));
+        expect(click).toBeCalled()
+        expect(screen.getByTestId('TopMessage-link1')).toHaveTextContent('contributing guideline')
+    });
+
+    test('second top link called', ()=> {
+        const click = jest.fn();
+        render(
+            <div data-testid='TopMessage-link2' onClick={click}>we've collected some good first issues for you.</div>
+        )
+        userEvent.click(screen.getByTestId('TopMessage-link2'));
+        expect(click).toBeCalled();
+        expect(screen.getByTestId('TopMessage-link2')).toHaveTextContent(/we've collected some/i)
     });
 
     test('matches snapshot', ()=> {
