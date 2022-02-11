cf032857432546f80b865a7700aed37f9130d35c
Omobolaji Koyi <omobolaji-oluwajuwon.koyi@deutschebahn.com>
2022-02-11 10:40:13 +0100

    style changes

--- src/components/issueList/issueList.tsx
+++ src/components/issueList/issueList.tsx
@@ -14,7 +14,11 @@ const IssueList = (prop: issueList) => {
 
     return (
         <div>{prop.node?.title? 
-            <div className={!hoverState && prop.index !== pagination.PageSize? 'issues-body' :  hoverState && prop.index !== pagination.PageSize? 'issues-body hover': hoverState && prop.index === pagination.PageSize? 'issues-body hover last-child' : !hoverState && prop.index === pagination.PageSize? 'issues-body last-child' : ''} onMouseEnter={()=>setHoverState(true)} onMouseLeave={()=>setHoverState(false)}>
+            <div className={!hoverState && prop.index !== pagination.PageSize? 'issues-body' :  
+                hoverState && prop.index !== pagination.PageSize? 'issues-body hover': 
+                hoverState && prop.index === pagination.PageSize? 'issues-body hover last-child' : 
+                !hoverState && prop.index === pagination.PageSize? 'issues-body last-child' : ''} 
+                onMouseEnter={()=>setHoverState(true)} onMouseLeave={()=>setHoverState(false)}>
                 <div className='issues-list'>
                     <span><i className={!prop.node.closed? "far fa-dot-circle":"far fa-check-circle"}></i></span>
                     <span className='title'><a href={prop.node?.url} target={'_blank'} rel="noreferrer">{prop.node?.title}</a></span>
@@ -23,10 +27,13 @@ const IssueList = (prop: issueList) => {
                     <span></span>
                     <span></span>
                     <span></span>
-                    <span>{prop.node?.comments.totalCount > 0 ? <span><i className="far fa-comment-alt"></i> {prop.node?.comments.totalCount}</span> : null}</span>
+                    <span>{prop.node?.comments.totalCount > 0 ? <span><i className="far fa-comment-alt">
+                        </i> {prop.node?.comments.totalCount}</span> : null}</span>
                 </div>
                 <div className='additional-info'>
-                    #{prop.node?.number} by <a href={prop.node?.author?.url? prop.node?.author?.url : 'https://github.com/reactjs/reactjs.org/issues'} target={'_blank'} rel="noreferrer">{(prop.node?.author?.name)?? 'User'}</a> was updated on {prop.node?.updatedAt.split('T')[0].split('-').reverse().join('-')} at {prop.node?.updatedAt.split('T')[1].slice(0, 8)}.
+                    #{prop.node?.number} by <a href={prop.node?.author?.url? prop.node?.author?.url : 
+                        'https://github.com/reactjs/reactjs.org/issues'} target={'_blank'} rel="noreferrer">
+                            {(prop.node?.author?.name)?? 'User'}</a> was updated on {prop.node?.updatedAt.split('T')[0].split('-').reverse().join('-')} at {prop.node?.updatedAt.split('T')[1].slice(0, 8)}.
                 </div>
             </div> : null
         
