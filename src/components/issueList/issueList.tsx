4cbb1c02e91e8bcc970a240adc9ec1be15052de8
Omobolaji Koyi <omobolaji-oluwajuwon.koyi@deutschebahn.com>
2022-03-02 08:59:52 +0100

    Code cleanup

--- src/components/issueList/issueList.tsx
+++ src/components/issueList/issueList.tsx
@@ -1,45 +1,82 @@
-import React, {useEffect, useState, } from 'react';
-import './issueList.css';
-import Loading from '../loading/loading';
-import { issueList, pagination } from '../../model/model';
-
+import React, { useState } from "react";
+import "./issueList.css";
+import Loading from "../loading/loading";
+import { issueList, pagination } from "../../model/model";
 
 const IssueList = (prop: issueList) => {
-    const [hoverState, setHoverState] = useState(false)
+  const [hoverState, setHoverState] = useState(false);
 
-    if(!prop){
-        return <Loading/>
-    }
-    
+  if (!prop) {
+    return <Loading />;
+  }
 
-    return (
-        <div>{prop.node?.title? 
-            <div className={!hoverState && prop.index !== pagination.PageSize? 'issues-body' :  
-                hoverState && prop.index !== pagination.PageSize? 'issues-body hover': 
-                hoverState && prop.index === pagination.PageSize? 'issues-body hover last-child' : 
-                !hoverState && prop.index === pagination.PageSize? 'issues-body last-child' : ''} 
-                onMouseEnter={()=>setHoverState(true)} onMouseLeave={()=>setHoverState(false)}>
-                <div className='issues-list'>
-                    <span><i className={!prop.node.closed? "far fa-dot-circle":"far fa-check-circle"}></i></span>
-                    <span className='title'><a href={prop.node?.url} target={'_blank'} rel="noreferrer">{prop.node?.title}</a></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span>{prop.node?.comments.totalCount > 0 ? <span><i className="far fa-comment-alt">
-                        </i> {prop.node?.comments.totalCount}</span> : null}</span>
-                </div>
-                <div className='additional-info'>
-                    #{prop.node?.number} by <a href={prop.node?.author?.url? prop.node?.author?.url : 
-                        'https://github.com/reactjs/reactjs.org/issues'} target={'_blank'} rel="noreferrer">
-                            {(prop.node?.author?.name)?? 'User'}</a> was updated on {prop.node?.updatedAt.split('T')[0].split('-').reverse().join('-')} at {prop.node?.updatedAt.split('T')[1].slice(0, 8)}.
-                </div>
-            </div> : null
-        
-        }
-            
+  return (
+    <div>
+      {prop.node?.title ? (
+        <div
+          className={
+            !hoverState && prop.index !== pagination.PageSize
+              ? "issues-body"
+              : hoverState && prop.index !== pagination.PageSize
+              ? "issues-body hover"
+              : hoverState && prop.index === pagination.PageSize
+              ? "issues-body hover last-child"
+              : !hoverState && prop.index === pagination.PageSize
+              ? "issues-body last-child"
+              : ""
+          }
+          onMouseEnter={() => setHoverState(true)}
+          onMouseLeave={() => setHoverState(false)}
+        >
+          <div className="issues-list">
+            <span>
+              <i
+                className={
+                  !prop.node.closed
+                    ? "far fa-dot-circle"
+                    : "far fa-check-circle"
+                }
+              ></i>
+            </span>
+            <span className="title">
+              <a href={prop.node?.url} target={"_blank"} rel="noreferrer">
+                {prop.node?.title}
+              </a>
+            </span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span>
+              {prop.node?.comments.totalCount > 0 ? (
+                <span>
+                  <i className="far fa-comment-alt"></i>{" "}
+                  {prop.node?.comments.totalCount}
+                </span>
+              ) : null}
+            </span>
+          </div>
+          <div className="additional-info">
+            #{prop.node?.number} by{" "}
+            <a
+              href={
+                prop.node?.author?.url
+                  ? prop.node?.author?.url
+                  : "https://github.com/reactjs/reactjs.org/issues"
+              }
+              target={"_blank"}
+              rel="noreferrer"
+            >
+              {prop.node?.author?.name ?? "User"}
+            </a>{" "}
+            was updated on{" "}
+            {prop.node?.updatedAt.split("T")[0].split("-").reverse().join("-")}{" "}
+            at {prop.node?.updatedAt.split("T")[1].slice(0, 8)}.
+          </div>
         </div>
-    )
-}
-export default IssueList
+      ) : null}
+    </div>
+  );
+};
+export default IssueList;
